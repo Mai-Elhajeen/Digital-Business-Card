e38c74d36f80bd0bd6aa6d1c7947d299a2af3cff
Mai-ELhajeen <maielhajeen2001@gmail.com>
2022-06-13 23:21:50 +0300

    adding component 4 code and style

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './style.css';
 import Option1 from './components/Option-1'
 import Option2 from './components/Option-2'
 import Option3 from './components/Option-3'
+import Option4 from './components/Option-4'
+
 
 function App() {
   return (
@@ -9,6 +11,7 @@ function App() {
       <Option1 />
       <Option2 />
       <Option3 />
+      <Option4 />
     </div>
   );
 }
