76dee82986c2640d2fa6f2fc944d92676218f366
Mai-ELhajeen <maielhajeen2001@gmail.com>
2022-06-13 23:11:00 +0300

    adding component 3 coding and styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './style.css';
 import Option1 from './components/Option-1'
 import Option2 from './components/Option-2'
+import Option3 from './components/Option-3'
+
 function App() {
   return (
     <div className="root">
       <Option1 />
       <Option2 />
+      <Option3 />
     </div>
   );
 }
